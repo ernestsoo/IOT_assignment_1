790ac86f8a33947af210b6df9f194264ba2607d4
ernestsoo <ernestsoo22@gmail.com>
2020-04-05 05:29:37 +0100

    Added OOP concepts and Code Commenting

--- taskB/monitorAndDisplay.py
+++ taskB/monitorAndDisplay.py
@@ -20,25 +20,41 @@ r = (255, 0, 0) #red
 b = (0, 181, 204) #blue
 g = (41, 241, 195) #green
 
-# Check temperature every 10 seconds and change pixels accordingly
-while True:
 
-  # Clear sensor
-  sense.clear()
+# Class Monitor to encapsulated or Monitoring and Displaying Functions
+class Monitor:
 
-  # Get temperature from sensor
-  temperature =  sense.get_temperature()  
- 
-  # Set colour of temperature display to the designated colours
-  if temperature < 10:
-   x = b	
-  elif temperature > 9 and temperature < 25:
-   x = g
-  elif temperature > 24:
-   x = r
+  # Function to get sense temperature  
+  def senseTemperature(self):
+    # Clear sensor
+    sense.clear()
 
-  # Show temperature with the assigned colour
-  sense.show_message(str(round(temperature,2)),0.2,x)
+    # Return read temperature
+    return sense.get_temperature()
 
-  # Refresh every 10 seconds
-  sleep(10)
+  # Functino to display temperature
+  def display(self):
+
+    # Check temperature every 10 seconds and change pixels accordingly 
+    while True:
+
+      # Set colour of temperature display to the designated colours
+      if self.senseTemperature() < config['cold_max']:
+        x = b	
+      elif self.senseTemperature() >= config['cold_max'] and self.senseTemperature() < config['comfortable_max']:
+        x = g
+      elif self.senseTemperature() >= config['hot_min']:
+        x = r
+
+      # Show temperature with the assigned colour
+      sense.show_message(str(round(self.senseTemperature(),2)),0.2,x)
+
+      # Refresh every 10 seconds
+      sleep(10)
+
+
+
+monitorInstance = Monitor()
+
+# Display Temperature
+monitorInstance.display()
